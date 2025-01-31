d71f9ed7cc3208e6f2ae039e2bdab0bb0e23c62f
Damianhch <daracha777@gmail.com>
2025-01-31 21:18:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post("/send-email", async (req, res) => {
 });
 
 // Server Listen
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
